215f367c68a7c7af3faed19700e3f0fd706a9f89
mohammadshuraim28 <mohammadshuraim192>
2024-03-28 13:54:02 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+
+const randomNumber = 13
+
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message:"please guess a number",
+}
+])
+if(answer.userGuessNumber === randomNumber){
+    console.log("congtratulation your answer is right");
+}else{
+    console.log("OHHH! YOUR ANSWER IS WRONG");
+}
